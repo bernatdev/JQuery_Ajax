90cfde5d33f9dd0b44e4075a4df46df0ceeb7ebe
bernatdev <61184540+bernatdev@users.noreply.github.com>
2020-06-17 12:35:15 +0200

    Exercici Ajax REST API
    Fer una crida a una Rest API mitjançant Jquery Ajax
    Chuck Norris jokes

---
+++ javascript.js
@@ -0,0 +1,22 @@
+$(document).ready(function () { // snippet jqDocReady
+    $('#ajax').click(function (e) {  // snippet jqClick: al hacer clic en el id # del documento, se ejecutará lo de más abajo
+        e.preventDefault();
+        $('#acudit').html(''); // jqHtmlSet para vaciar el #id cada vez que hacemos clic 
+        $.ajax({
+            url:'http://api.icndb.com/jokes/random',
+            type:'get',
+            dataType:'json',
+            success:function(data){
+                console.log(data);
+                $.each(data, function (i, item) {  // snippet jqEach para leer cada item de la colección data json
+                     $('#acudit').html(item.joke); // jqHtmlSet para introducir los datos del item json en el #id 
+                });
+            },
+            error:function(xhr,status,error){
+                console.log(xhr);
+                console.log(status);
+                console.log(error);
+            }
+        });
+    });
+});
