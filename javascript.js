8f8d5c7369ab8d60819b40760396c331331314d3
bernatdev <61184540+bernatdev@users.noreply.github.com>
2020-06-17 13:00:04 +0200

    Funció per carregar acudit
    Ara l'ajax està dins una funció, que es crida al carregar la pàgina i al fer clic

--- javascript.js
+++ javascript.js
@@ -1,22 +1,27 @@
+function buscarAcudit() {
+
+    $.ajax({
+        url: 'http://api.icndb.com/jokes/random',
+        type: 'get',
+        dataType: 'json',
+        success: function (data) {
+            console.log(data);
+            $.each(data, function (i, item) {  // snippet jqEach para leer cada item de la colección data json
+                $('#acudit').html(item.joke); // jqHtmlSet para introducir los datos del item json en el #id 
+            });
+        },
+        error: function (xhr, status, error) {
+            console.log(xhr);
+            console.log(status);
+            console.log(error);
+        }
+    });
+}
+
 $(document).ready(function () { // snippet jqDocReady
     $('#ajax').click(function (e) {  // snippet jqClick: al hacer clic en el id # del documento, se ejecutará lo de más abajo
         e.preventDefault();
         $('#acudit').html(''); // jqHtmlSet para vaciar el #id cada vez que hacemos clic 
-        $.ajax({
-            url:'http://api.icndb.com/jokes/random',
-            type:'get',
-            dataType:'json',
-            success:function(data){
-                console.log(data);
-                $.each(data, function (i, item) {  // snippet jqEach para leer cada item de la colección data json
-                     $('#acudit').html(item.joke); // jqHtmlSet para introducir los datos del item json en el #id 
-                });
-            },
-            error:function(xhr,status,error){
-                console.log(xhr);
-                console.log(status);
-                console.log(error);
-            }
-        });
+        buscarAcudit();
     });
 });
